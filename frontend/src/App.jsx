f73e4d31daad2885121b03ddb17543c25e1936ee
Khadimrass <khadimd978@gmail.com>
2026-09-12 20:34:06 +0300

    Ajout du style CSS complet au site

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import './App.css'
 import Login from './Login'
 import FormulaireEnfant from './FormulaireEnfant'
 import FormulairePaiement from './FormulairePaiement'
@@ -41,38 +42,64 @@ function App() {
   }
 
   return (
-    <div>
-      <Login />
-      <FormulaireEnfant onEnfantCree={handleNouvelEnfant} />
+    <div className="app">
+      <div className="barre-haut">
+        <div className="bloc-login">
+          <h4>Connexion</h4>
+          <Login />
+        </div>
+        <div className="bloc-formulaire">
+          <h4>Ajouter un enfant</h4>
+          <FormulaireEnfant onEnfantCree={handleNouvelEnfant} />
+        </div>
+      </div>
+
       <h1>Liste des enfants</h1>
-      <ul>
+      <ul className="conteneur-enfants">
         {enfants.map(enfant => (
-          <li key={enfant.id}>
-            {enfant.nom}
+          <li
+            key={enfant.id}
+            className={
+              enfantSelectionne && enfantSelectionne.id === enfant.id
+                ? 'carte-enfant selectionnee'
+                : 'carte-enfant'
+            }
+          >
+            <h3>{enfant.nom}</h3>
             <button onClick={() => setEnfantSelectionne(enfant)}>Voir détails</button>
           </li>
         ))}
       </ul>
+
       {enfantSelectionne && (
-        <div>
+        <div className="details-enfant">
           <h2>Détails de {enfantSelectionne.nom}</h2>
 
           <h3>Bulletins</h3>
           <FormulaireBulletin enfantId={enfantSelectionne.id} onBulletinCree={handleNouveauBulletin} />
-          <ul>
+          <ul className="liste-items">
             {bulletins.filter(b => b.enfant_id === enfantSelectionne.id).map(b => (
-              <li key={b.id}>
-  {b.annee_scolaire} - {b.classe}
-  <img src={"http://127.0.0.1:8000/" + b.fichier_bulletin_url} width="100" />
-</li>
+              <li key={b.id} className="item-carte">
+                {b.fichier_bulletin_url && (
+                  <img
+                    src={"http://127.0.0.1:8000/" + b.fichier_bulletin_url}
+                    width="50"
+                    height="50"
+                    alt="bulletin"
+                  />
+                )}
+                <span>{b.annee_scolaire} - {b.classe}</span>
+              </li>
             ))}
           </ul>
 
           <h3>Paiements</h3>
           <FormulairePaiement enfantId={enfantSelectionne.id} onPaiementCree={handleNouveauPaiement} />
-          <ul>
+          <ul className="liste-items">
             {paiements.filter(p => p.enfant_id === enfantSelectionne.id).map(p => (
-              <li key={p.id}>{p.annee_scolaire} - {p.tranche} - {p.montant} FCFA</li>
+              <li key={p.id} className="item-carte">
+                {p.annee_scolaire} - {p.tranche} - {p.montant} FCFA
+              </li>
             ))}
           </ul>
         </div>
